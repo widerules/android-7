ab4351ac75b3981ce417bf817f2d322eedfafcee
Jenny900925 <Jenny900925@gmail.com>
2012-02-14 12:55:58 +0000

    DB관련

--- WhenWhereWho3/src/kr/co/WhenWhereWho3/MyWishListActivity.java
+++ WhenWhereWho3/src/kr/co/WhenWhereWho3/MyWishListActivity.java
@@ -1,15 +1,89 @@
 package kr.co.WhenWhereWho3;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MyWishListActivity extends Activity {
-    /** Called when the activity is first created. */
+
+	private MovieDBHelper dbHelper;
+	private SQLiteDatabase db;
+	
+	Movie movie;
+	ArrayList<Movie> movies;
+	
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.mymovielist);
-    }//	onCreate( ) ³¡
+        super.onCreate(savedInstanceState); 
+        setContentView(R.layout.mywishlist);
+        
+        dbHelper = new MovieDBHelper(this);
+        db = dbHelper.getWritableDatabase();
+        movies = new ArrayList<Movie>();
+        
+        Cursor cursor = db.rawQuery("SELECT * FROM t_wishlist", null);
+
+        if(cursor!=null) {
+        	ListView listview = (ListView)findViewById(R.id.listview);
+        	getMyWishListInfo(cursor);
+        	MovieListAdapter adapter = new MovieListAdapter(MyWishListActivity.this, R.layout.searchlist, movies);
+        	listview.setAdapter(adapter);
+        }
+    }
+    
+    public void getMyWishListInfo(Cursor outCursor) {
+    	int recordCnt = outCursor.getCount();
+    	
+    	String[] photo_Col = {"m_photo_1", "m_photo_2", "m_photo_3", "m_photo_4", "m_photo_5"};
+    	String[] actors = null;
+    	String[] photos = null;
+    	
+    	int m_titleCol = outCursor.getColumnIndex("m_title");
+    	int m_thumbnailCol = outCursor.getColumnIndex("m_thumbnail");
+    	int m_nationCol = outCursor.getColumnIndex("m_nation");
+    	int m_directorCol = outCursor.getColumnIndex("m_director");
+    	int m_actorCol = outCursor.getColumnIndex("m_actor");
+    	int m_genreCol = outCursor.getColumnIndex("m_genre");
+    	int m_open_infoCol = outCursor.getColumnIndex("m_open_info");
+    	int m_gradeCol = outCursor.getColumnIndex("m_grade");
+    	int m_photo_1Col = outCursor.getColumnIndex("m_photo_1");
+    	int m_photo_2Col = outCursor.getColumnIndex("m_photo_2");
+    	int m_photo_3Col = outCursor.getColumnIndex("m_photo_3");
+    	int m_photo_4Col = outCursor.getColumnIndex("m_photo_4");
+    	int m_photo_5Col = outCursor.getColumnIndex("m_photo_5");
+    	int m_storyCol = outCursor.getColumnIndex("m_story");
+    	
+    	for(int i=0; i<recordCnt; i++) {
+    		outCursor.moveToNext();
+    		
+    		movie = new Movie();
+
+    		movie.setTitle(outCursor.getString(m_titleCol));   
+    		movie.setThumbnail(outCursor.getString(m_thumbnailCol));
+    		movie.setNation(outCursor.getString(m_nationCol));
+    		movie.setDirector(outCursor.getString(m_directorCol));
+    		//movie.setActor();
+    		movie.setGenre(outCursor.getString(m_genreCol));
+    		movie.setOpenInfo(outCursor.getString(m_open_infoCol));
+    		movie.setGrade(outCursor.getString(m_gradeCol));
+    		//movie.setPhoto();
+    		
+    		for(int j=0; j>5; j++) {
+    			
+    		}
+    		movie.setStory(outCursor.getString(m_storyCol));
+    				
+    		movies.add(movie);
+    	
+    	}
+    }
 }
 
 
--- WhenWhereWho3/src/kr/co/WhenWhereWho3/SearchMovieDetailActivity.java
+++ WhenWhereWho3/src/kr/co/WhenWhereWho3/SearchMovieDetailActivity.java
@@ -1,11 +1,14 @@
 package kr.co.WhenWhereWho3;
 
 import java.util.Arrays;
-
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.TypedArray;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -21,19 +24,20 @@ import android.widget.TextView;
 public class SearchMovieDetailActivity extends Activity {	
 
 	private final ImageDownloader imageDownloader = new ImageDownloader();
+	private MovieDBHelper dbHelper;
+	private SQLiteDatabase db;
+	
+	Movie movie;
 	
 	TextView movieTitle;
 	TextView movieNation;
-	
 	ImageView movieImg;
 	TextView movieDirector;
 	TextView movieActors;
 	TextView movieGenre;
 	TextView movieOpenInfo;
-	
 	Gallery galleryPhoto;
 	TextView movieStory;
-	
 	String [] photo;
 	
 	Intent intent;
@@ -59,8 +63,7 @@ public class SearchMovieDetailActivity extends Activity {
 		btnWishList.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
-				
+				request();
 			}
 		});
 		
@@ -77,7 +80,7 @@ public class SearchMovieDetailActivity extends Activity {
 		Intent intent = getIntent();
 		
 		if(intent != null) {
-			Movie movie = (Movie)intent.getSerializableExtra("movie");
+			movie = (Movie)intent.getSerializableExtra("movie");
 			
 			movieTitle.setText(movie.getTitle());
 			movieNation.setText("(" + movie.getNation() + ")");
@@ -102,6 +105,72 @@ public class SearchMovieDetailActivity extends Activity {
 				startActivity(dialog);
 			}
 		});
+	}
+	
+	public void insertMovieInfo() {
+		dbHelper = new MovieDBHelper(this);
+		db = dbHelper.getWritableDatabase();
+		
+		ContentValues recordValues = new ContentValues();
+		
+		String[] photo = movie.getPhoto();
+		String[] actors = movie.getActor();
+		String actor = "";
+		
+		for(int i=0; i<actors.length; i++) {
+			actor += actors[i] + ", ";
+		}
+		
+		recordValues.put("m_title", movie.getTitle());
+		recordValues.put("m_thumbnail", movie.getThumbnail());
+		recordValues.put("m_nation", movie.getNation());
+		recordValues.put("m_director", movie.getDirector());
+		recordValues.put("m_actor", actor);
+		recordValues.put("m_genre", movie.getGenre());
+		recordValues.put("m_open_info", movie.getOpenInfo());
+		recordValues.put("m_grade", movie.getGrade());
+		recordValues.put("m_photo_1", photo[0]);
+		recordValues.put("m_photo_2", photo[1]);
+		recordValues.put("m_photo_3", photo[2]);
+		recordValues.put("m_photo_4", photo[3]);
+		recordValues.put("m_photo_5", photo[4]);
+		recordValues.put("m_story", movie.getStory());
+		
+		db.insert("t_wishlist", null, recordValues) ;
+	}
+	
+	public void request() {
+		String title = "찜하기";
+		String message = "찜 하시겠습니까?";
+		String titleButtonYes = "예";
+		String titleButtonNo = "아니오";
+		
+		AlertDialog dialog = makeRequestDialog(title, message, titleButtonYes, titleButtonNo);
+		dialog.show();
+	}
+	
+	private AlertDialog makeRequestDialog(CharSequence title, CharSequence message, 
+			CharSequence titleButtonYes, CharSequence titleButtonNo ) {
+		
+		AlertDialog.Builder requestDialog = new AlertDialog.Builder(this);
+		requestDialog.setTitle(title);
+		requestDialog.setMessage(message);
+		requestDialog.setPositiveButton(titleButtonYes, new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				insertMovieInfo();
+			}
+		});
+		requestDialog.setNegativeButton(titleButtonNo, new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				
+			}
+		});
+		
+		return requestDialog.show();
 	}
 	
 	public class UserGalleryAdapter extends BaseAdapter {
@@ -151,6 +220,5 @@ public class SearchMovieDetailActivity extends Activity {
 			return view;
 		}
 	}
-
 }
 
